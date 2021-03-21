372fecf1e6862178041823e8f19c59858ae8bf61
mynameisfwk <2239657654@qq.com>
2021-03-22 04:31:00 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -11,127 +11,122 @@ import success from '@/pages/pay/success'
 import parts from '@/pages/shops/parts'
 import phone from '@/pages/shops/phone'
 import collection from '@/pages/user/collections/index'
-
-// 整理路由中 分割线～
-// import xz_address from "@/pages/user/children/xz_address"
-// import view from '@/pages/view/view'
 import order from '@/pages/user/order/index'
 import orderDetail from '@/pages/user/order/detail'
-// import OrderDetails from '@/pages/order/orderView'
-// import choose from '@/pages/choose'
-// import news from '@/pages/News/news'
-// import newsView from '@/pages/News/newsView'
-
-// import classify from "@/pages/Classify/classify"
 import pay from "@/pages/pay/pay"
-
-
-// import add_address from "@/pages/user/children/add_address"
-// import shopDetail from "@/pages/shop/shopDetail"
 import login from "@/pages/user/login"
 import register from "@/pages/user/register"
-// import upaddress from "@/pages/user/children/upaddress"
-// import cart from '@/pages/user/children/cart'
 import main from "@/pages/user/main"
-// import collection from "@/pages/user/children/collection"
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    { 
-      path: '/', 
-      name: 'index', 
-      component: index 
+    {
+      path: '/',
+      name: 'index',
+      component: index
     },
-
-    { 
-      path: '/index', 
-      name: 'index', 
-      component: index 
+    {
+      path: '/index',
+      name: 'index',
+      component: index
     },
-
-    { 
-      path: '/detail', 
-      name: 'detail', 
-      component: detail 
+    {
+      path: '/detail',
+      name: 'detail',
+      component: detail
     },
-
-    { 
-      path: '/news', 
-      name: 'news', 
-      component: news 
+    {
+      path: '/news',
+      name: 'news',
+      component: news
     },
-
-    { 
-      path: '/newsDetail', 
-      name: 'newsDetail', 
+    {
+      path: '/newsDetail',
+      name: 'newsDetail',
       component: newsDetail
     },
-
-    { 
-      path: '/cart', 
-      name: 'cart', 
-      component: cart 
+    {
+      path: '/cart',
+      name: 'cart',
+      component: cart,
+      meta: {
+        requireAuth: true
+      },
     },
-
     {
       path: '/address',
-      name:'address',
-      component: address
+      name: 'address',
+      component: address,
+      meta: {
+        requireAuth: true
+      },
     },
-
     {
       path: '/add_address',
-      name:'add_address',
+      name: 'add_address',
       component: add_address
     },
-    
     {
       path: '/success',
-      name:'success',
+      name: 'success',
       component: success
     },
-
-
-    { path: '/login', name: 'login', component: login },
-    // { path: '/view', name: 'view', component: view },
-    // { path: '/xz_address', name: 'xz_address', component: xz_address },
-   
-    { path: '/register', name: 'register', component: register },
     { 
-      path: '/main', 
-      name: 'main', 
+      path: '/login',
+      name: 'login', 
+      component: login 
+    },
+    { 
+      path: '/register', 
+      name: 'register',
+      component: register 
+    },
+    {
+      path: '/main',
+      name: 'main',
       component: main,
       meta: {
         requireAuth: true
       },
     },
-    { path: '/collection', name: 'collection', component: collection },
-
+    { path: '/collection', 
+      name: 'collection', 
+      component: collection 
+    },
     {
       path: '/order',
       name: 'order',
       component: order,
+      meta: {
+        requireAuth: true
+      },
     },
-    { path: '/orderDetail', name: 'orderDetail', component: orderDetail },
-    // { path: '/choose', name: 'choose', component: choose },
-    // { path: '/newsView', name: 'newsView', component: newsView },
-
-    // { path: '/news', name: 'news', component: news },
-    { path: '/parts', name: 'parts', component: parts },
-    { path: '/phone', name: 'phone', component: phone },
-    // { path: '/change', name: 'change', component: change },
-    // { path: '/classify', name: 'classify', component: classify },
+    { 
+      path: '/orderDetail', 
+      name: 'orderDetail', 
+      component: orderDetail,
+      meta: {
+        requireAuth: true
+      },
+    },
+    { 
+      path: '/parts',
+      name: 'parts', 
+      component: parts 
+    },
+    { 
+      path: '/phone', 
+      name: 'phone', 
+      component: phone 
+    },
     {
       path: '/pay',
       name: 'pay',
-      component: pay
+      component: pay,
+      meta: {
+        requireAuth: true
+      },
     }
-   
-    // {
-    //   path: '/shopDetail',
-    //   name: 'shopDetail',
-    //   component: shopDetail
-    // }
   ]
 })
