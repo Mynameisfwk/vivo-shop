a011975b6b6b18b0609abbeb8b7c8826155f7fd9
mynameisfwk <2239657654@qq.com>
2021-03-26 03:10:32 +0800

    Vant组件

--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -113,9 +113,9 @@
           <van-goods-action>
             <van-goods-action-icon icon="chat-o" text="客服" dot />
             <van-goods-action-icon icon="cart-o" text="购物车" :badge="$store.state.cart.carts.length" />
-            <van-goods-action-icon icon="star-o" size="2rem" @click="addCollection(list)" text="收藏" />
-            <van-goods-action-button type="warning" @click="addCart(list)" text="加入购物车" />
-            <van-goods-action-button type="danger"  @click="jumpPay(list)" text="立即购买" />
+            <van-goods-action-icon icon="star-o" text="收藏" @click="addCollection(list)"  />
+            <van-goods-action-button type="warning" text="加入购物车" @click="addCart(list)"/>
+            <van-goods-action-button type="danger" text="立即购买" @click="jumpPay(list)"  />
           </van-goods-action>
         </li>
       </ul>
